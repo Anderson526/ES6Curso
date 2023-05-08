cae23ca05e082f26c071449a3195cbf651d5e850
Anderson Chila <1megaword1@gmail.com>
2023-05-08 09:09:58 -0500

    new commit first excercise to js arrow function implicit

---
+++ js/00-js-arrowfunction.js
@@ -0,0 +1,15 @@
+//normal function
+function square(num){
+    return num*num;
+}
+
+//arrow function
+const square=(num) =>{
+    return num* num;
+}
+
+
+//arrow funcion implicit 
+
+const square = (num)=>num*num;
+console.log(square(2));
